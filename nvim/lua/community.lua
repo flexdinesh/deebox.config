03573f3a9416036526458194eb9b0c56c6921007
Dinesh Pandiyan <flexdinesh@gmail.com>
2024-12-19 22:41:51 +1100

    remove unnecessary plugins aand set up statusline

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -10,14 +10,15 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   -- { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
   -- { import = "astrocommunity.bars-and-lines.statuscol-nvim" },
   { import = "astrocommunity.color.vim-highlighter" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.dracula-nvim" },
-  { import = "astrocommunity.colorscheme.gruvbox-baby" },
+  -- { import = "astrocommunity.colorscheme.dracula-nvim" },
+  -- { import = "astrocommunity.colorscheme.gruvbox-baby" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.editing-support.cutlass-nvim" },
+  -- { import = "astrocommunity.editing-support.cutlass-nvim" },
   { import = "astrocommunity.editing-support.vim-move" },
   { import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
   { import = "astrocommunity.fuzzy-finder.fzf-lua" },
@@ -31,9 +32,9 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.recipes.auto-session-restore" },
-  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  -- { import = "astrocommunity.scrolling.neoscroll-nvim" },
   { import = "astrocommunity.utility.hover-nvim" },
-  { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
+  -- { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
   -- example of importing an entire language pack
   -- these packs can set up things such as Treesitter, Language Servers, additional language specific plugins, and more!
   -- { import = "astrocommunity.pack.rust" },
